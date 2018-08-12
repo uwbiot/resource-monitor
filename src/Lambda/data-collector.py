00297392687a4697b3377b1969d6e6b58ab04764
cjmediran <16298278+cjmediran@users.noreply.github.com>
2018-08-11 22:46:31 -0700

    saved cpu, memory, and disk info to dynamoDB

--- src/Lambda/data-collector.py
+++ src/Lambda/data-collector.py
@@ -4,6 +4,7 @@ import os
 import json
 import subprocess
 import shlex
+from os import statvfs
 
 from datetime import datetime
 
@@ -16,103 +17,116 @@ my_platform = platform.platform()
 def get_memory_info():
     """"""
     fd = open('/proc/meminfo').readlines()
-    memory_info = dict((i.split()[0].rstrip(':'), int(i.split()[1])) for i in fd)
+    memory_info = dict((i.split()[0].rstrip(':'), str(i.split()[1])) for i in fd)
 
     total_mem = memory_info['MemTotal']
     free_mem = memory_info['MemFree']
     avail_mem = memory_info['MemAvailable']
 
-    # publish information to the cloud
-    #client.publish(
-    #    topic='RM/Greengrass/data/MemTotal',
-    #    payload=json.dumps({'MemTotal': total_mem})
-    #)
-    #client.publish(
-    #    topic='RM/Greengrass/data/MemFree',
-    #    payload=json.dumps({'MemFree': free_mem})
-    #)
-    #client.publish(
-    #    topic='RM/Greengrass/data/MemAvailable',
-    #    payload=json.dumps({'MemAvailable': avail_mem})
-    #)
     return memory_info
 
 # ------------------------------------------------------------------------------
 def get_cpu_info():
     """"""
     fd = open('/proc/cpuinfo').readlines()
-    #print fd
     fd = [_.replace("\n", "").replace("\t","") for _ in fd ]
+    
     cpu_info = {}
     info_list = {}
     processor = ""
+    
     for item in fd:
         data = item.split(":")
         desc = data[0]
         
         if len(data) == 2:
             info = data[1].lstrip()
-        else:
-            info = data[0], ""
-        
+
         if desc == "processor":
             processor = info
         else:
             info_list[desc] = info
             
         if desc == "power management":
+            if '' in info_list:
+                del info_list['']
+                
+            info_list[desc] = "n/a"
             cpu_info["processor " + processor] = info_list
             
-    print "cpu_info"
-    print cpu_info
+    for c in cpu_info:
+        if '' in cpu_info[c]:
+            del cpu_info[c]['']
+
     return cpu_info
-
-
 
 # ------------------------------------------------------------------------------
 def get_hostname():
-    fd = subprocess.Popen(shlex.split('hostname'), stdout=subprocess.PIPE, shell=True)
-    hostname, _ = fd.communicate()
-    print(hostname)
-    client.publish( # publish information to the cloud
-        topic='RM/Greengrass/data/deviceId',
-        payload=json.dumps({'deviceId': hostname})
-    )
+    try: 
+        hostname = subprocess.check_output("hostname", stderr=subprocess.STDOUT, shell=True)
+    except Exception as err:
+        hostname = err.output
+    
+    hostname = "Greengrass"
     return hostname
 
 # ------------------------------------------------------------------------------
 def get_disk_info():
-    df = subprocess.Popen(shlex.split('df -h'), stdout=subprocess.PIPE, shell=True)
-    dfdata, _ = df.communicate()
-    dfdata = dfdata.replace("Mounted on", "Mounted_on")
-    print "disk_info "
-    print dfdata
-    return dfdata
+
+    with open("/proc/mounts", "r") as mounts:
+        split_mounts = [s.split() for s in mounts.read().splitlines()]
+
+    data = str()
+    
+    for p in split_mounts:
+        stat = statvfs(p[1])
+        block_size = stat.f_bsize
+        blocks_total = stat.f_blocks
+        blocks_free = stat.f_bavail
+
+        size_mb = float(blocks_total * block_size) / 1024 / 1024
+        free_mb = float(blocks_free * block_size) / 1024 / 1024
+
+        data += "{0:24} {1:24} {2:16} {3:16} {4:10.2f}MiB {5:10.2f}MiB ".format(
+                p[0], p[1], blocks_total, blocks_free, size_mb, free_mb)
+
+    data = [ _ for _ in data.split(" ") if _ is not '']
+
+    disk_data = []
+    desc = ["FS", "Mountpoint", "Blocks", "Blocks_Free", "Size", "Free"]
+    info = {}
+        
+    #for item in 
+    for i, item in enumerate( data ):
+        if i % 6 == 0:
+            disk_data.append(info)
+        else:
+            info[ desc[ i % 6 ] ] = item
+
+    return disk_data
 
 # ------------------------------------------------------------------------------
 def get_cpu_temp():
-    cpu_temp = subprocess.Popen(shlex.split('vcgencmd measure_temp'), stdout=subprocess.PIPE, shell=True)
-    cpu_temp, _ = cpu_temp.communicate()
-    print "cpu_temp " 
-    print cpu_temp
-    client.publish(
-        topic='RM/Greengrass/data/Cpu_Temp',
-        payload=json.dumps({'cpu_temp': cpu_temp})
-    )
-    return cpu_temp#.split('=')[1]
+    #cpu_temp = subprocess.Popen(shlex.split('vcgencmd measure_temp'), stdout=subprocess.PIPE, shell=True)
+    #cpu_temp, _ = cpu_temp.communicate()
+    try:
+        cpu_temp = subprocess.check_output("vcgencmd measure_temp", stderr=subprocess.STDOUT, shell=True)
+    except Exception as err:
+        cpu_temp = err.output
+
+    return cpu_temp
 
 
 # ------------------------------------------------------------------------------
 def get_lambdas():
-    lambdas = subprocess.Popen(shlex.split('ls /greengrass/ggc/deployment/lambda'), stdout=subprocess.PIPE, shell=True)
-    lambdas, _ = lambdas.communicate()
-    print "lamdas"
-    print lambdas
-    client.publish(
-        topic='RM/Greengrass/data/Lambdas',
-        payload=json.dumps({'Lambdas': lambdas})
-    )
-    return lambdas
+    lambdas = "/greengrass/ggc/deployment/lambda does not exist"
+    try:
+        lambdas = os.listdir("/greengrass/ggc/deployment/lambda")
+        lambdas = [ _ for _ in lambdas if not _.startswith('.')]
+    except Exception as err:
+        print err
+   
+    return lambdas  
 
 # ------------------------------------------------------------------------------
 def run():
@@ -137,7 +151,7 @@ def run():
         )
     else: #greengrass platform
         client.publish(
-            topic='RM/Greengrass/data',
+            topic="RM/{}/data".format(hostname),
             payload=json.dumps({
                 'deviceId': hostname,
                 'timestamp': date_time,
